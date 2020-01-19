48f59e2852465814e2b84166215e54f54976ed79
Arnaud Peralta <arnaud.peralta@gmail.com>
2020-01-19 22:57:50 +0100

    Board clickable

--- src/graphic/board.py
+++ src/graphic/board.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from tkinter import ttk
+import math
 
+BOARD_SIZE = 400
+BORDER_SIZE = 5
+CELLS_BY_LINE = 5
 # Classe héritant de la classe Frame de tkinter
 
 
@@ -13,15 +17,52 @@ class Board(ttk.Frame):
         frame_left.pack(side=LEFT)
         frame_right = Frame(self.master)
         frame_right.pack(side=RIGHT)
-        self.board_left = Canvas(frame_left.master, width=400, height=400)
+        self.board_left = Canvas(frame_left.master
+                                 , width=BOARD_SIZE + 2 * BORDER_SIZE, height=BOARD_SIZE + 2 * BORDER_SIZE)
         self.board_left.pack(side=LEFT)
-        self.board_right = Canvas(frame_right.master, width=400, height=400)
+        self.board_right = Canvas(frame_right.master
+                                  , width=BOARD_SIZE + 2 * BORDER_SIZE, height=BOARD_SIZE + 2 * BORDER_SIZE)
         self.board_right.pack(side=RIGHT)
 
     def draw(self):
-        draw_grid(self.board_left)
-        draw_grid(self.board_right)
+        self.draw_grid(self.board_left, "left")
+        self.draw_grid(self.board_right, "right")
+
+    def draw_grid(self, canvas, name):
+        canvas.create_rectangle(BORDER_SIZE, BORDER_SIZE, BOARD_SIZE + BORDER_SIZE, BOARD_SIZE + BORDER_SIZE)
+        cell_size = BOARD_SIZE / CELLS_BY_LINE
+        # Tracé des lignes
+        for i in range(1, CELLS_BY_LINE):
+            canvas.create_line(i * cell_size + BORDER_SIZE, BORDER_SIZE
+                               , i * cell_size + BORDER_SIZE, BORDER_SIZE + BOARD_SIZE)
+        for i in range(1, CELLS_BY_LINE):
+            canvas.create_line(BORDER_SIZE, i * cell_size + BORDER_SIZE
+                               , BORDER_SIZE + BOARD_SIZE, i * cell_size + BORDER_SIZE)
+        # Mapping des cellules
+        for i in range(1, CELLS_BY_LINE):
+            for j in range(1, CELLS_BY_LINE):
+                if name == "left":
+                    canvas.bind("<Button-1>", self.boardleft_callback)
+                else:
+                    canvas.bind("<Button-1>", self.boardright_callback)
+
+    def boardleft_callback(self, event):
+        self.paint_cell(self.board_left, cell_coords(event.x, event.y))
+
+    def boardright_callback(self, event):
+        self.paint_cell(self.board_right, cell_coords(event.x, event.y))
 
 
-def draw_grid(canvas):
-    canvas.create_rectangle(5, 5, 400, 400)
+def paint_cell(self, canvas, coords):
+    cell_size = BOARD_SIZE / CELLS_BY_LINE
+    canvas.create_rectangle(BORDER_SIZE + (coords[0]) * cell_size
+                            , BORDER_SIZE + (coords[1]) * cell_size
+                            , BORDER_SIZE + (coords[0] + 1) * cell_size
+                            , BORDER_SIZE + (coords[1] + 1) * cell_size
+                            , fill="red")
+
+
+# Transforme des coordonnées canvas en couple x,y de coords cellules
+def cell_coords(x, y):
+    return [math.floor((x - BORDER_SIZE) * CELLS_BY_LINE / BOARD_SIZE),
+            math.floor((y - BORDER_SIZE) * CELLS_BY_LINE / BOARD_SIZE)]
